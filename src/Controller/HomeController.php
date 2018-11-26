78f2118b8c1d17ee18c7d7f9d673587a72cbfba3
Idiallo <iboudiallo84@gmail.com>
2018-11-27 00:50:31 +0100

    Homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,12 +7,20 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    
+ 
     public function index()
     {
          
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
+    }
+
+    /**
+     * @Route("contact-us", name="home_contact")
+     *      
+     */
+    public function contact(){
+        return $this->render('contact/contact.html.twig');
     }
 }
