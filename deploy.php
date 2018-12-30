9de8e1c0adbdb8c6a5a575e474d053127b457e6f
Ibrahima D <iboudiallo84@gmail.com>
2018-12-30 21:03:18 +0100

    test 8

--- deploy.php
+++ deploy.php
@@ -50,7 +50,7 @@ host('root@51.77.201.108')
 // Tasks
 
 task('build', function () {
-    run('cd {{release_path}} && composer install --no-dev --optimize-autoloader');
+    //run('cd {{release_path}} && composer install --no-dev --optimize-autoloader');
     run('cd {{release_path}} && npm install');
     run('./node_modules/.bin/encore production');
     run('{{bin/php}} {{console}} cache:warmup --env=prod --no-debug --no-interaction');
