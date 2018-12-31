03b855b14f1af4a88bcfba3e4f232a99e9f4c02d
Ibrahima D <iboudiallo84@gmail.com>
2018-12-31 03:48:52 +0100

    Test Dep

--- deploy.php
+++ deploy.php
@@ -2,7 +2,7 @@
 
 namespace Deployer;
 
-require 'recipe/symfony4.php';
+require 'recipe/symfony.php';
 
 // Project name
 set('application', 'deployed_community');
@@ -13,35 +13,13 @@ set('repository', 'git@github.com:ibou/community.git');
 // [Optional] Allocate tty for git clone. Default value is false.
 set('git_tty', true);
 
-set('ssh_type', 'native');
-set('ssh_multiplexing', true);
-set('symfony_env', 'prod');
-set('shared_dirs', ['var/log', 'var/sessions']);
-set('shared_files', ['.env']);
-// set('writable_dirs', ['var']);
-set('bin_dir', 'bin');
-set('var_dir', 'var');
-set('keep_releases', 2);
-set('bin/console', function () {
-    return sprintf('{{release_path}}/%s/console', trim(get('bin_dir'), '/'));
-});
-set('console_options', function () {
-    $options = '--no-interaction --env={{symfony_env}}';
+// Shared files/dirs between deploys
+add('shared_files', []);
+add('shared_dirs', []);
 
-    return get('symfony_env') !== 'prod' ? $options : sprintf('%s --no-debug', $options);
-});
-set('env', function () {
-    return [
-        'SYMFONY_ENV' => get('symfony_env'),
-    ];
-});
-set('clear_paths', [
-    '.git',
-    '.gitignore',
-]);
+// Writable dirs by web server
+add('writable_dirs', []);
 
-set('http_user', 'www-data');
-set('writable_mode', 'chmod');
 // Hosts
 
 host('root@51.77.201.108')
@@ -53,17 +31,13 @@ task('build', function () {
     run('cd {{release_path}} && composer install --no-dev --optimize-autoloader && npm install && ./node_modules/.bin/encore production');
 });
 
-after('deploy:update_code', 'deploy:clear_paths');
-after('deploy:vendors', 'deploy:writable');
-// after('deploy:failed', 'deploy:unlock');
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
 
+// Migrate database before symlink new release.
 // Migrate database before symlink new release.
 task('database:migrate', function () {
     run('{{bin/php}} {{bin/console}} doctrine:schema:update --force {{console_options}}');
 })->desc('Migrate database');
 
-task('runbuild', [
-    'build',
-]);
-after('deploy:vendors', 'runbuild');
 before('deploy:symlink', 'database:migrate');
