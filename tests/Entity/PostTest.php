88b1ef543488e33c7962851d20f252e0ea22aa28
Ibrahima D <iboudiallo84@gmail.com>
2019-01-01 03:51:24 +0100

    Test git prepush after phpunit

--- tests/Entity/PostTest.php
+++ tests/Entity/PostTest.php
@@ -41,7 +41,7 @@ class PostTest extends TestCase
         $reflection = new \ReflectionClass($this->object);
         $property = $reflection->getProperty('id');
         $property->setAccessible(true);
-        $property->setValue($this->object, 900);
+        $property->setValue($this->object, 950);
         $this->assertEquals(9, $this->object->getId());
     }
 }
