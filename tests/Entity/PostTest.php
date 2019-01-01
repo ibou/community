01688ad6060e05b2667ab709d16fdb3815164f01
Ibrahima D <iboudiallo84@gmail.com>
2019-01-01 03:59:31 +0100

    Final test prepush whit phpunit before final

--- tests/Entity/PostTest.php
+++ tests/Entity/PostTest.php
@@ -41,7 +41,7 @@ class PostTest extends TestCase
         $reflection = new \ReflectionClass($this->object);
         $property = $reflection->getProperty('id');
         $property->setAccessible(true);
-        $property->setValue($this->object, 970);
+        $property->setValue($this->object, 9);
         $this->assertEquals(9, $this->object->getId());
     }
 }
