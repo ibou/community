62d0e62aca2057fa23aca5cd7e3584e1880409e0
ibrahima Diallo <ibrahima.diallo@wolterskluwer.com>
2019-02-27 18:39:13 +0100

    style page

--- assets/js/comments_likes.js
+++ assets/js/comments_likes.js
@@ -5,16 +5,16 @@ const axios = require('axios');
 function onClickBtnLike(event){
     event.preventDefault();
     const url = this.href;
-    const spanCount = this.querySelector('span.js-likes');  
+    const spanCount = this.querySelector('span.js-likes');
 
-    const icon = this.querySelector('i');   
-    axios.get(url) 
-    .then(function(response){ 
-        spanCount.textContent = response.data.likes; 
-        
-        if(icon.classList.contains('fa-thumbs-down')){ 
+    const icon = this.querySelector('i');
+    axios.get(url)
+    .then(function(response){
+        spanCount.textContent = response.data.likes;
+
+        if(icon.classList.contains('fa-thumbs-down')){
             icon.classList.replace('fa-thumbs-down','fa-thumbs-up');
-        }else{ 
+        }else{
             icon.classList.replace('fa-thumbs-up','fa-thumbs-down');
         }
 
@@ -31,9 +31,8 @@ function onClickBtnLike(event){
       .then(function () {
         // always executed
       });
-} 
+}
 
- document.querySelectorAll('a.js-like').forEach(function(link){  
-     link.addEventListener('click',  onClickBtnLike);    
+ document.querySelectorAll('a.js-like').forEach(function(link){
+     link.addEventListener('click',  onClickBtnLike);
  })
- 
