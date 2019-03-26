c01a82e3efb40b7d1d20b83fc66126fe85f0d071
ibrahima Diallo <ibrahima.diallo@wolterskluwer.com>
2019-03-26 18:08:03 +0100

    Test unit

--- tests/Controller/ApplicationAvailabilityFunctionalTest.php
+++ tests/Controller/ApplicationAvailabilityFunctionalTest.php
@@ -4,19 +4,34 @@ namespace App\Tests;
 
 use Symfony\Bundle\FrameworkBundle\Tests\TestCase;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Symfony\Component\BrowserKit\Cookie;
+use App\Entity\Post;
 
 class ApplicationAvailabilityFunctionalTest extends WebTestCase
 {
+
+    private $client = null;
+
+    public function setUp()
+    {
+        $this->client = static::createClient( );
+        $this->client->followRedirects(true);
+    }
+
     /**
      * @dataProvider urlProvider
      */
     public function testPageIsSuccessful(string $url)
     {
         $this->assertTrue(true);
-        $client = static::createClient();
-        $client->request('GET', $url);
-
-        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->client->request('GET', $url);
+         $this->assertSame(
+            Response::HTTP_OK,
+            $this->client->getResponse()->getStatusCode(),
+            sprintf('The %s public URL loads correctly.', $url)
+        );
     }
 
     public function urlProvider()
@@ -26,5 +41,13 @@ class ApplicationAvailabilityFunctionalTest extends WebTestCase
         yield ['/register'];
         yield ['/contact-us'];
         yield ['/login/reset-password'];
+    }
+    public function testPublicBlogPost()
+    {
+        // the service container is always available via the test client
+        $blogPost = $this->client->getContainer()->get('doctrine')->getRepository(Post::class)->find(104);
+
+        $this->client->request('GET', sprintf('/posts/article/%s', $blogPost->getSlug()));
+        $this->assertSame(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
     }
 }
