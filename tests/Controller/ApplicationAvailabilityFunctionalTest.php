b639bd2bde0f9882b30a94be2a663986c11118cb
ibrahima Diallo <ibrahima.diallo@wolterskluwer.com>
2019-03-26 14:44:02 +0100

    test routes

--- tests/Controller/ApplicationAvailabilityFunctionalTest.php
+++ tests/Controller/ApplicationAvailabilityFunctionalTest.php
@@ -3,21 +3,28 @@
 namespace App\Tests;
 
 use Symfony\Bundle\FrameworkBundle\Tests\TestCase;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class ApplicationAvailabilityFunctionalTest extends TestCase
+class ApplicationAvailabilityFunctionalTest extends WebTestCase
 {
-    public function testPageIsSuccessful()
+    /**
+     * @dataProvider urlProvider
+     */
+    public function testPageIsSuccessful(string $url)
     {
         $this->assertTrue(true);
-        // $client = self::createClient();
-        // $client->request('GET', $url);
+        $client = static::createClient();
+        $client->request('GET', $url);
 
-        // $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertTrue($client->getResponse()->isSuccessful());
     }
 
     public function urlProvider()
     {
         yield ['/'];
         yield ['/login'];
+        yield ['/register'];
+        yield ['/contact-us'];
+        yield ['/login/reset-password'];
     }
 }
