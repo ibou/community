c18631531d43e582c1728af959e4ac6ae0b36f73
ibrahima Diallo <ibrahima.diallo@wolterskluwer.com>
2019-04-10 11:05:49 +0200

    right command docker exec without options

--- bin/git-pre-push.sh
+++ bin/git-pre-push.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker exec -it sf4_php_apache ./vendor/bin/simple-phpunit --coverage-text --coverage-html web/coverage
+docker exec sf4_php_apache ./vendor/bin/simple-phpunit --coverage-text --coverage-html web/coverage
--- bin/run-test.sh
+++ bin/run-test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker exec -it sf4_php_apache ./vendor/bin/simple-phpunit --coverage-text --coverage-html web/coverage
+docker exec sf4_php_apache ./vendor/bin/simple-phpunit --coverage-text --coverage-html web/coverage
