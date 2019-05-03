5838f7de4754309173d408e99db8ca728bf66519
Ibrahima D <iboudiallo84@gmail.com>
2019-05-03 21:59:35 +0200

    translations!

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -16,26 +16,26 @@ use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 class UserType extends AbstractType
 {
 
- public function buildForm(FormBuilderInterface $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('firstname', TextType::class)
             ->add('lastname', TextType::class)
             ->add('email', EmailType::class)
-            ->add('username', TextType::class) 
-        ;
+            ->add('username', TextType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
             'data_class' => User::class,
-             'csrf_protection' => true,
+            'csrf_protection' => true,
             // the name of the hidden HTML field that stores the token
             'csrf_field_name' => '_token',
             // an arbitrary string used to generate the value of the token
             // using a different string for each form improves its security
             'csrf_token_id'   => 'user_type',
+            'translation_domain' => 'forms',
         ]);
     }
 }
